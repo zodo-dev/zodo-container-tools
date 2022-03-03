6323adb48833eeff3c292a57b847adf1787da51d
Edimar <edimarlnx@gmail.com>
2022-03-03 20:17:16 -0300

    Meteor 2.6.1 container

---
+++ meteor/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export NODE_VERSION=16
+export METEOR_VERSION=2.6.1
+export CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-""}
+export IMAGE_NAME=meteor
+export IMAGE_TAG=${METEOR_VERSION}
+export IMAGE_NAME_TAG="${CONTAINER_REGISTRY}${IMAGE_NAME}:${IMAGE_TAG}"
+docker build . -t "${IMAGE_NAME_TAG}"
+docker push "${IMAGE_NAME_TAG}"
