a40d0a17ab7aebe58d55efae53e2186de07fdb7a
Edimar <edimarlnx@gmail.com>
2022-03-03 21:10:13 -0300

    Meteor 2.6.1 container

--- meteor/build.sh
+++ meteor/build.sh
@@ -4,8 +4,19 @@ export METEOR_VERSION=2.6.1
 export CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-""}
 export IMAGE_NAME=meteor
 export IMAGE_TAG=${METEOR_VERSION}
-export IMAGE_NAME_TAG="${CONTAINER_REGISTRY}${IMAGE_NAME}:${IMAGE_TAG}"
+export IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_TAG}"
+export PUBLISH_GH=${PUBLISH_GH:-false}
+export PUBLISH_DOCKER_HUB=${PUBLISH_DOCKER_HUB:-false}
 
 echo "IMAGE_NAME_TAG: ${IMAGE_NAME_TAG}"
 docker build . -t "${IMAGE_NAME_TAG}"
-docker push "${IMAGE_NAME_TAG}"
+if [ "${PUBLISH_GH}" == "true" ]; then
+  echo Publishing on Github
+  docker tag "${IMAGE_NAME_TAG}" "${GH_REGISTRY}${IMAGE_NAME_TAG}"
+  docker push "${GH_REGISTRY}${IMAGE_NAME_TAG}"
+fi
+if [ "${PUBLISH_DOCKER_HUB}" == "true" ]; then
+  echo Publishing on Docker Hub
+  docker tag "${IMAGE_NAME_TAG}" "zododev/${IMAGE_NAME_TAG}"
+  docker push "zododev/${IMAGE_NAME_TAG}"
+fi
